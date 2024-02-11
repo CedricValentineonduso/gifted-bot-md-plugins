c4503305a8c9c22a1ab5be51d6a47091a41c4bdd
giftedtech <103584295+Giftedmaurice@users.noreply.github.com>
2024-02-11 16:37:44 +0300

    areact.js

--- plugins/areact.js
+++ plugins/areact.js
@@ -25,16 +25,16 @@ const mojis = ['💘','💝','💖','💗','💓','💞','💕','💟','❣️',
 
 CURRENTLY RUNNING ON BETA VERSION!!
 *
-   * @project_name : Suhail-Md
-   * @author : Suhail Tech Info
-   * @youtube : https://www.youtube.com/c/@SuhailTechInfo
-   * @infoription : Suhail-Md ,A Multi-functional whatsapp user bot.
+   * @project_name : Gifted-Tech
+   * @author : Gifted Tech
+   * @youtube : https://www.youtube.com/c/2giftedtechnexus
+   * @infoription :Gifted-Tech ,A Multi-functional whatsapp user bot.
    * @version 1.2.2 
 *
    * Licensed under the  GPL-3.0 License;
 * 
-   * ┌┤Created By Suhail Tech Info.
-   * © 2023 Suhail-Md ✭ ⛥.
+   * ┌┤Created By Gifted Tech.
+   * © 2024 Gifted-Tech ✭ ⛥.
    * plugin date : 10/6/2023
 * 
    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
