fc68c89b3d368ddbd9747d9792ffa562d76005b0
giftedtech <103584295+Giftedmaurice@users.noreply.github.com>
2024-02-15 17:22:48 +0300

    ɢɪғᴛᴇᴅ ᴛᴇᴄʜ
    ɢɪғᴛᴇᴅ ᴛᴇᴄʜ

--- plugins/anticall.js
+++ plugins/anticall.js
@@ -1,36 +1,16 @@
-
-/**
-
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                                      //
-//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
-//                                                                                                      // 
-//                                         Ｖ：1．2．2                                                   // 
-//                                                                                                      // 
-//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
-//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
-//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
-//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
-//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
-//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
-//                                                                                                      //
-//                                                                                                      //
-//                                                                                                      //
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-
 CURRENTLY RUNNING ON BETA VERSION!!
 *
-   * @project_name : Gifted-Tech
+   * @project_name : gifted-md
    * @author : Gifted Tech
    * @youtube : https://www.youtube.com/c/@giftedtechnexus
-   * @infoription : Gifted-Tech ,A Multi-functional whatsapp user bot.
+   * @infoription : Gifted-Md ,A Multi-functional whatsapp user bot.
    * @version 1.2.2 
 *
    * Licensed under the  GPL-3.0 License;
 * 
    * ┌┤Created By Gifted Tech.
    * © 2024 Gifted-Bot-Md ✭ ⛥.
-   * plugin date : 10/6/2023
+   * plugin date : 14/2/2024
 * 
    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
