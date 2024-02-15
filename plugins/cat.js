6d3aa1823f02de00fda6bc46616fe15eb3800397
giftedtech <103584295+Giftedmaurice@users.noreply.github.com>
2024-02-15 17:57:17 +0300

    ɢɪғᴛᴇᴅ ᴛᴇᴄʜ
    ɢɪғᴛᴇᴅ ᴛᴇᴄʜ

--- plugins/cat.js
+++ plugins/cat.js
@@ -1,35 +1,17 @@
 /**
-
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                                      //
-//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
-//                                                                                                      // 
-//                                         Ｖ：1．2．2                                                   // 
-//                                                                                                      // 
-//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
-//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
-//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
-//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
-//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
-//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
-//                                                                                                      //
-//                                                                                                      //
-//                                                                                                      //
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
-
 CURRENTLY RUNNING ON BETA VERSION!!
 *
-   * @project_name : Gifted-tech
+   * @project_name : gifted-md
    * @author : GiftedTech <https://github.com/Giftedmaurice>
    * @youtube : https://www.youtube.com/c/@giftedtechnexus
-   * @infoription : Gifted-Tech ,A Multi-functional whatsapp user bot.
+   * @infoription : Gifted-Md ,A Multi-functional whatsapp user bot.
    * @version 1.2.5 
 *
    * Licensed under the  GPL-3.0 License;
 * 
    * ┌┤Created By Gifted Tech.
-   * © 2024 Gifted-Tech ✭ ⛥.
-   * plugin date : 10/12/2023
+   * © 2024 Gifted-Md ✭ ⛥.
+   * plugin date : 14/2/2024
 * 
    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -61,13 +43,13 @@ const {
 
 smd({
     cmdname: "cat",
-    desc: "Send Images of randome Cats!",
+    desc: "Send Images of random Cats!",
     type: "misc",
     filename: __filename,
   },
   async(m)=>{
   try{
-    await m.send('https://cataas.com/cat',{caption:"*meyaoooooooooooooon!*"},"img",m)
+    await m.send('https://cataas.com/cat',{caption:"*meyaoooooooooooooou!*"},"img",m)
   }catch(e){ m.error(`${e}\n\nCommand: cat`,e,false)}
   })
   
